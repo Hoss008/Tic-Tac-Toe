874f18b394af91b39c819581cec97fa0ba8db7b0
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2025-10-08 23:50:19 +0300

    player function

--- script.js
+++ script.js
@@ -26,8 +26,25 @@ const gameBoard = (function () {
   return { mark, isEmpty, reset, boardArray };
 })();
 
-gameBoard.mark(3,"X")
-gameBoard.mark(4,"O")
-// gameBoard.mark(4,"X")
-gameBoard.reset()
-console.log(gameBoard.boardArray);
+
+function createPlayer(name, marker) {
+  function getName() {
+    return name;
+  }
+
+  function getMarker() {
+    return marker;
+  }
+
+  return { getName, getMarker };
+}
+
+const player1 = createPlayer('Player 1', 'X');
+const player2 = createPlayer('Player 2', 'O');
+
+
+// gameBoard.mark(3,"X")
+// gameBoard.mark(4,"O")
+// // gameBoard.mark(4,"X")
+// gameBoard.reset()
+// console.log(gameBoard.boardArray);
