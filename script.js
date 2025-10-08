19dd79cb53a45535d43e2e2cb16252807d7c04e4
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2025-10-08 22:58:29 +0300

    finished with the game board logic

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//pattern module
+const gameBoard = (function () {
+  const boardArray = ["", "", "", "", "", "", "", "", ""]; 
+
+  function mark(position, marker) {
+    if (isEmpty(position)) {
+      return boardArray[position]=marker;
+    } else {
+      console.log("invalid position");
+    }
+  }
+
+  function isEmpty(position) {
+    if (boardArray[position] == '') {
+      return boardArray[position] == '';
+    } else {
+      return false;
+    }
+  }
+
+  function reset() {
+    for(let i=0;i<boardArray.length;i++){
+      boardArray[i]='';
+    }
+  }
+
+  return { mark, isEmpty, reset, boardArray };
+})();
+
+gameBoard.mark(3,"X")
+gameBoard.mark(4,"O")
+gameBoard.mark(4,"X")
+gameBoard.reset()
+console.log(gameBoard.boardArray);
